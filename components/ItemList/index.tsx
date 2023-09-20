02c183ed4c9d1141ad9b013d0db93624a3386894
Hnin Thet Hmue <hninthethmue8@gmail.com>
2023-09-20 22:03:41 +0630

    ItemList responsive

--- components/ItemList/index.tsx
+++ components/ItemList/index.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Item from "../Item";
+import { BiChevronRight } from "react-icons/bi";
+import clsx from "clsx";
 
 interface ItemListProps {}
 
@@ -42,18 +47,42 @@ const itemlist_data = [
 ];
 
 const ItemList: React.FC<ItemListProps> = () => {
+  const [width, setWidth] = useState(0);
+  const isMobile = width < 400;
+
+  const getItemList = () => {
+    const data = [...itemlist_data];
+    if(isMobile){
+      return data.splice(0,2);
+    }
+    return data;
+  }
+
+  useEffect(() => {
+    if(window.innerWidth){
+      setWidth(window.innerWidth)
+    }
+  },[])
   return (
-    <div className="w-full md:w-[1110px] mt-[3rem] text-center mb-[5rem]">
+    <div className="w-full md:w-[1110px] mt-[1rem] md:mt-[3rem] text-center mb-[5rem]">
       <h3 className="font-bold text-[45px]">Best Deals</h3>
       <p className="text-[20px] mb-[30px]">Just For You</p>
       <div className="w-full grid grid-cols-2 md:grid-cols-6 gap-[6px] px-5">
         {
-            itemlist_data.map(item => (
+            getItemList().map(item => (
                 <Item {...item} key={item.name} />
             ))
         }
         
-        
+      </div>
+      <div className="w-full flex justify-center">
+        <button className={clsx(
+          "flex gap-1 text-md bg-gray-200 text-black/75 items-center rounded-full",
+          "px-[5rem] py-[0.5rem]"
+        )}>
+          View More
+          <BiChevronRight/>
+        </button>
       </div>
     </div>
   );
